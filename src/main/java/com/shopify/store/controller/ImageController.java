21eeedeb85d8cf99ed9bc965c9c81be6630772e8
Johnson <breaking.lover@gmail.com>
2021-04-30 13:55:30 -0400

    Added error handling

--- src/main/java/com/shopify/store/controller/ImageController.java
+++ src/main/java/com/shopify/store/controller/ImageController.java
@@ -4,14 +4,17 @@ import com.shopify.store.model.Image;
 import com.shopify.store.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
 
-@RestController
+@Controller
 @RequestMapping("api/v1/image")
 public class ImageController {
 
@@ -28,13 +31,16 @@ public class ImageController {
     }
 
     @PostMapping("upload")
-    public ResponseEntity<String> uploadImage(
+    public String uploadImage(
             @RequestParam("imageFile") MultipartFile[] imageFile,
             @RequestParam("image_privacy") String image_privacy,
+            RedirectAttributes redirectAttrs,
             Principal principal
     ) {
         boolean isPublic = (image_privacy.equals("true"))?true:false;
-        return imageService.saveImage(imageFile, principal.getName(), isPublic);
+        List<String> response = imageService.saveImage(imageFile, principal.getName(), isPublic);
+        redirectAttrs.addFlashAttribute("response", response);
+        return "redirect:/addImage";
     }
 
     @DeleteMapping("delete/{id}")
